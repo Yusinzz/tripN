b6d9949287d59935c36d52720fabe528818b651b
niuee <vntchang@gmail.com>
2023-08-08 16:33:03 +0800

    Initial Setup Commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'devDist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx'],
+  },
+  module: {
+    
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/playground/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'), // Specify the directory for serving static files
+  },
+};
